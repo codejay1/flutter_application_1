25d994277aa839309bca5c1d5cd79aaae9de87fa
Jay <akinjidesewanu@gmail.com>
2024-10-23 09:32:49 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# flutter_application_1
+
+A new Flutter project.
+
+## Getting Started
+
+This project is a starting point for a Flutter application.
+
+1. download flutter sdk
+2. Install flutter sdk
+3. setup visual studio code or andriod emmulator
+4. create a folder for your project
+5. run your app
+
---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main(){
+
+runApp(Myapp());
+}
+
+class Myapp extends StatelessWidget{
+
+  Widget build(BuildContext context){
+
+  return MaterialApp(home:Center(child: Text('My experience with flutter has been a good and funny one, because the environment is new, famillar with the framework but never used it, but i am happy i get to use it, thanks to the AIP. '),),);
+
+
+  }
+}
